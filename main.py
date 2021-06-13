58ed6b9186621cc1ae188ad6e0c1d74d9d181211
IN22913438 <bharanidharan.kaliyaperumal@tesco.com>
2021-06-13 09:06:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+df = pd.read_excel("./nism_registration.xlsx")
+df = df[df.to_be_registered=='Yes']
+registration_data = df.to_dict(orient='records')
+
+
+def fillGender(driver, data):
+    try:
+        if data['gender'] == 'M':
+            Select(driver.find_element_by_id('ddlTitle')).select_by_index(1)
+            Select(driver.find_element_by_id('ddlGender')).select_by_index(1)
+
+        else:
+            Select(driver.find_element_by_id('ddlTitle')).select_by_index(2)
+            Select(driver.find_element_by_id('ddlGender')).select_by_index(2)
+    except:
+        pass
+
+
+def fillContactDetails(driver, data):
+    try:
+        address = data['address']
+        address_lines = address.split(',')
+        Select(driver.find_element_by_id('ddlCountry')).select_by_index(1)
+        driver.find_element_by_id('txtAddress1').send_keys((address_lines[0] + address_lines[1]).strip())
+        driver.find_element_by_id('txtAddress2').send_keys(address_lines[2].strip())
+        driver.find_element_by_id('txtAddress3').send_keys(address_lines[-1].strip())
+        driver.find_element_by_id('txtCity').send_keys(data['city'])
+        driver.find_element_by_id('txtPinCode').send_keys(data['pincode'])
+        Select(driver.find_element_by_id('ddlState')).select_by_index(32)
+        driver.find_element_by_id('txtMobile').send_keys(data['mobile'])
+    except:
+        pass
+
+
+def fillPersonalDetails(driver, data):
+    try:
+        driver.find_element_by_id('txtEmailId').send_keys(data['email'])
+        driver.find_element_by_id('txtConfirmEmailId').send_keys(data['email'])
+        driver.find_element_by_id('txtPassword').send_keys(data['password'])
+        driver.find_element_by_id('txtConfirmPassword').send_keys(data['password'])
+        fillGender(driver, data)
+        driver.find_element_by_id('txtFirstName').send_keys(data['firstName'])
+        driver.find_element_by_id('txtLastName').send_keys(data['lastName'])
+        driver.find_element_by_id('txtFatherName').send_keys(data['fatherName'])
+        driver.find_element_by_id('txtDateOfBirth').send_keys(data['dob'])
+        driver.find_element_by_id('txtPan').send_keys(data['pan'])
+        driver.find_element_by_id('txtConfirmPan').send_keys(data['pan'])
+        driver.find_element_by_id('strAadhaarNumber').send_keys(data['aadhaar'])
+    except:
+        pass
+
+
+def fillEducationDetails(driver, data):
+    try:
+        if data['education'].lower() == 'graduation':
+            Select(driver.find_element_by_id('ddlQualification')).select_by_index(3)
+            if data['major'].lower() == 'commerce':
+                Select(driver.find_element_by_id('ddlMajorSubject')).select_by_index(3)
+            driver.find_element_by_id('txtGradInstitute').send_keys(data['university'])
+            Select(driver.find_element_by_id('ddlGradYearofPassing')).select_by_visible_text(str(data['yop']))
+            driver.find_element_by_id('txtGradPercenteage').send_keys(str(data['percentage']))
+    except:
+        pass
+
+
+def fillExperienceDetails(driver, data):
+    try:
+        if data['occupation'].lower() == 'employed':
+            Select(driver.find_element_by_id('ddlOccupation')).select_by_index(2)
+            if data['major'].lower() == 'commerce':
+                Select(driver.find_element_by_id('ddlMajorSubject')).select_by_index(3)
+            if data['country_office'].lower() == 'india':
+                Select(driver.find_element_by_id('ddlOfficeCountry')).select_by_visible_text(data['country_office'])
+            driver.find_element_by_id('txtNameOfOrganization').send_keys(data['organization'])
+            driver.find_element_by_id('txtDesignation').send_keys(data['designation'])
+            driver.find_element_by_id('txtFromDate').send_keys(data['from_date'].strftime("%d/%m/%Y"))
+            driver.find_element_by_id('txtOfficeAdd1').send_keys(data['address_office'])
+            driver.find_element_by_id('txtOfficeCity').send_keys(data['city_office'])
+            driver.find_element_by_id('txtOfficePinCode').send_keys(data['pincode_office'])
+            if data['phone']:
+                driver.find_element_by_id('txtEmpSTD').send_keys(data['phone'].split('-')[0])
+                driver.find_element_by_id('txtEmpPhoneNo').send_keys(data['phone'].split('-')[1])
+            if data['country_office'].lower() == 'india':
+                Select(driver.find_element_by_id('ddlOfficeState')).select_by_visible_text(data['state_office'])
+    except:
+        pass
+
+driver = webdriver.Chrome('./chromedriver')
+
+for i in range(len(registration_data)):
+    driver.get("https://certifications.nism.ac.in/nismaol/Candidate/UserRegistration.aspx")
+    data = registration_data[i]
+    fillPersonalDetails(driver, data)
+    fillContactDetails(driver, data)
+    fillEducationDetails(driver, data)
+    fillExperienceDetails(driver, data)
+    if i < len(registration_data)-1:
+        driver.execute_script(f"window.open('about:blank', 'tab'+{i});")
+        driver.switch_to.window(f"tab{i}")
+
+
